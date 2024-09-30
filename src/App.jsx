d2e85152c059b747aac245b47ff773eadbb98f27
Ttlesgo <monish.e7@gmail.com>
2024-09-30 10:45:39 +0530

    Monish image added

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ const ContributorGrid = () => {
   const contributors = [
     // { name: "Anantesh G", imageUrl: "/images/pfp.jpg" },
     // Add more contributors here
+    {name:"Monish E",imageUrl:"public/images/img1.jpg"},
     ...Array(49).fill({ name: "", imageUrl: "" }) // Fill the rest with empty boxes
   ];
 
